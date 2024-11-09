40216797aa446d957bd9dde65fe0fdd73cd29746
jay valiya <valiyajay555@gmail.com>
2024-11-09 08:49:50 +0530

    added zod

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,145 +1,149 @@
-const { google } = require('googleapis')
-const oauth2Client = require("../config/googleConfig")
+const { google } = require('googleapis');
+const oauth2Client = require('../config/googleConfig');
 
-const GuardianModel = require('../models/guardianModel')
-const DoctorModel = require('../models/doctorModel')
+const GuardianModel = require('../models/guardianModel');
+const DoctorModel = require('../models/doctorModel');
+import { loginSchema } from '../zod';
 
-const { verifyOtp, sendOtpViaMail } = require('../services/otpService')
+const { verifyOtp, sendOtpViaMail } = require('../services/otpService');
 
-async function getGoogleUserDetail (code){
+async function getGoogleUserDetail(code) {
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
 
-  const { tokens } = await oauth2Client.getToken(code)
-  oauth2Client.setCredentials(tokens)
+  const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2' });
 
-  const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2'})
+  console.log(` oauth2 is =>> ${JSON.stringify(oauth2.userinfo)}`);
 
-  
-
-  console.log(` oauth2 is =>> ${JSON.stringify(oauth2.userinfo)}`)
-
-  const { data } = await oauth2.userinfo.get()
-  console.log(`data is ${JSON.stringify(data)}`)
-  return data
+  const { data } = await oauth2.userinfo.get();
+  console.log(`data is ${JSON.stringify(data)}`);
+  return data;
 }
 
 async function createAccount(model, email, password) {
-  const existingAccount = await model.findOne({ email })
-  if (existingAccount) 
-    throw new Error("An account with the provided email already exists")
-  
+  const existingAccount = await model.findOne({ email });
+  if (existingAccount)
+    throw new Error('An account with the provided email already exists');
+
   const newAccount = new model({
     email,
-    password
-  })
-  await newAccount.save()
-  
-  return newAccount
+    password,
+  });
+  await newAccount.save();
+
+  return newAccount;
 }
 
 async function sendGuardianSignupOtp(req, res) {
   try {
-    const { email, password }= req.body
+    const { email, password } = req.body;
 
-    const existingRecord = await GuardianModel.findOne({ email })
-    if(existingRecord) 
-      throw new Error("An account with the provided email already exists")
+    const existingRecord = await GuardianModel.findOne({ email });
+    if (existingRecord)
+      throw new Error('An account with the provided email already exists');
 
-    await sendOtpViaMail(email, password)
+    await sendOtpViaMail(email, password);
 
-    return res.status(200).json({ message: "OTP sent to the provided email"})
-
+    return res.status(200).json({ message: 'OTP sent to the provided email' });
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
 }
 async function sendDoctorSignupOtp(req, res) {
   try {
-    const { email, password }= req.body
+    const { email, password } = req.body;
 
-    const existingRecord = await DoctorModel.findOne({ email })
-    if(existingRecord) 
-      throw new Error("An account with the provided email already exists")
+    const existingRecord = await DoctorModel.findOne({ email });
+    if (existingRecord)
+      throw new Error('An account with the provided email already exists');
 
-    await sendOtpViaMail(email, password)
+    await sendOtpViaMail(email, password);
 
-    return res.status(200).json({ message: "OTP sent to the provided email"})
-
+    return res.status(200).json({ message: 'OTP sent to the provided email' });
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
 }
-async function verifyOtpAndCreateGuardian (req, res) {
+async function verifyOtpAndCreateGuardian(req, res) {
   try {
-    const { email, otp}= req.body
+    const { email, otp } = req.body;
 
-    const verifiedOtpRecord  = await verifyOtp(email, otp)
+    const verifiedOtpRecord = await verifyOtp(email, otp);
 
-    const createdAccount = await createAccount(GuardianModel, verifiedOtpRecord.email, verifiedOtpRecord.password)
-    const accessToken = await createdAccount.generateAccessToken()
+    const createdAccount = await createAccount(
+      GuardianModel,
+      verifiedOtpRecord.email,
+      verifiedOtpRecord.password
+    );
+    const accessToken = await createdAccount.generateAccessToken();
 
-    return res.status(200).json({ accessToken, message: "Guardian account created successfully" })
-
+    return res
+      .status(200)
+      .json({ accessToken, message: 'Guardian account created successfully' });
   } catch (error) {
-    return res.status(400).json({ message: error.message })
+    return res.status(400).json({ message: error.message });
   }
 }
 
-async function verifyOtpAndCreateDoctor (req, res) {
+async function verifyOtpAndCreateDoctor(req, res) {
   try {
-    const { email, otp }= req.body
+    const { email, otp } = req.body;
 
-    const verifiedOtpRecord = await verifyOtp(email, otp)
+    const verifiedOtpRecord = await verifyOtp(email, otp);
 
-    const createdAccount = await createAccount(DoctorModel, verifiedOtpRecord.email, verifiedOtpRecord.password)
-    const accessToken = await createdAccount.generateAccessToken()
+    const createdAccount = await createAccount(
+      DoctorModel,
+      verifiedOtpRecord.email,
+      verifiedOtpRecord.password
+    );
+    const accessToken = await createdAccount.generateAccessToken();
 
-    return res.status(200).json({ accessToken, message: "Guardian account created successfully" })
-
+    return res
+      .status(200)
+      .json({ accessToken, message: 'Guardian account created successfully' });
   } catch (error) {
-    return res.status(400).json({ message: error.message })
+    return res.status(400).json({ message: error.message });
   }
 }
-
 
 async function guardianLogin(req, res) {
-  try{
-    const { email, password }= req.body
-    if(!email || !password)
-      throw new Error("Email and Password are required")
+  try {
+    const valid = loginSchema.safeParse(req.body);
+    if (!valid.success) {
+      return res.status(400).json({ message: valid.error.message });
+    }
 
-    const guardian = await GuardianModel.findOne({ email })
-    if (!guardian) 
-      throw new Error('No account found with this email')
+    const { email, password } = req.body;
+    if (!email || !password) throw new Error('Email and Password are required');
 
-    await guardian.validatePassword(password)
+    const guardian = await GuardianModel.findOne({ email });
+    if (!guardian) throw new Error('No account found with this email');
 
-    const accessToken = await guardian.generateAccessToken()
+    await guardian.validatePassword(password);
 
-    return res.status(200).json({ accessToken, message: "Login successfully" })
+    const accessToken = await guardian.generateAccessToken();
 
+    return res.status(200).json({ accessToken, message: 'Login successfully' });
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
 }
 
 async function doctorLogin(req, res) {
-  try{
-    const { email, password }= req.body
-    if(!email || !password)
-      throw new Error("Email and Password are required")
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) throw new Error('Email and Password are required');
 
-    const doctor = await DoctorModel.findOne({ email })
-    if (!doctor) 
-      throw new Error('No account found with this email')
+    const doctor = await DoctorModel.findOne({ email });
+    if (!doctor) throw new Error('No account found with this email');
 
-    await doctor.validatePassword(password)
+    await doctor.validatePassword(password);
 
-    const accessToken = await doctor.generateAccessToken()
+    const accessToken = await doctor.generateAccessToken();
 
-    return res.status(200).json({ accessToken, message: "Login successfully" })
-
+    return res.status(200).json({ accessToken, message: 'Login successfully' });
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
 }
 
@@ -149,5 +153,5 @@ module.exports = {
   verifyOtpAndCreateGuardian,
   verifyOtpAndCreateDoctor,
   guardianLogin,
-  doctorLogin
-}
+  doctorLogin,
+};
